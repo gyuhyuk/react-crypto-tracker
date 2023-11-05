6b23b0a6b8a9e831c0b67a4c0345e76e180cc90c
gyuhyuk <bruno3@naver.com>
2023-11-05 23:00:24 +0900

    #5.4 Route States

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -33,12 +33,17 @@ const Coins = () => {
         <Title>코인</Title>
       </Header>
       {loading ? (
-        "loading..."
+        "Loading..."
       ) : (
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link to={`/${coin.id}`} state={{name:coin.name}}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
@@ -75,13 +80,20 @@ const Coin = styled.li`
   margin-bottom: 10px;
   border-radius: 15px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
       color: ${(props) => props.theme.accentColor};
     }
   }
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
