867067d80a9ce6500ddd25ebf882bea68edc3a97
gyuhyuk <bruno3@naver.com>
2023-11-08 01:53:10 +0900

    #5.10 React Query Part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Router from "./Router";
 import GlobalStyle from "./style/GlobalStyle";
+import { ReactQueryDevtools } from "react-query/devtools";
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 };
